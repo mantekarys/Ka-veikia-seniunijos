42797b941f13afd2cd477de0104947a1df454888
79539642 <patrik.voicechovski@ktu.edu>
2022-02-26 23:22:18 +0200

    Signup init

--- Ka-veikia-seniunijos/ClientApp/src/components/Home.js
+++ Ka-veikia-seniunijos/ClientApp/src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Login from './Login/Login';
+import Signup from './Signup/Signup';
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-        <Login/>
+        <Signup/>
     );
   }
 }
---
+++ Ka-veikia-seniunijos/ClientApp/src/components/Signup/Signup.jsx
@@ -0,0 +1,23 @@
+﻿import React, { useState } from 'react';
+import Popup from '../Popup/Popup';
+import '../style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+export default function Signup() {
+    const [userType, setUserType] = useState('');
+    const [isUserTypeSelected, setIsUserTypeSelected] = useState(false);
+
+    return (
+        <Popup>
+            <div className='login__container'>
+                <FontAwesomeIcon className='form__icon' icon={faXmark} />
+                <h2 className='header__secondary u-text-center'>Registracija</h2>
+
+                <div className='signup__type-container'>
+                    <h2 className='header__secondary u-text-center'>Pasirinkiti registracijos tipą</h2>
+                </div>
+            </div>
+        </Popup>
+    );
+}

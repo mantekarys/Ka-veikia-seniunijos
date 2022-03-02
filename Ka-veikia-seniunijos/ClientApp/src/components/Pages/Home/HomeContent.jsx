66ef8a69f77d1e9833b1c37396bae886942d14c4
79539642 <patrik.voicechovski@ktu.edu>
2022-03-02 20:52:46 +0200

    Sidebar updates

--- Ka-veikia-seniunijos/ClientApp/src/components/Pages/Home/HomeContent.jsx
+++ Ka-veikia-seniunijos/ClientApp/src/components/Pages/Home/HomeContent.jsx
@@ -6,10 +6,11 @@ import { GlobalContext } from '../../Pages/Home/Context/GlobalState';
 export default function HomeContent() {
     const { state, toggleSidebar } = useContext(GlobalContext);
     const areas = ['Vilnius', 'Kaunas', 'Klaipėda'];
-
+    
     return (
         <div className='home__content'>
-            {state.isSidebarOpen && <Sidebar onClose={toggleSidebar}/>}
+            {state.isSidebarOpen && <Sidebar onClose={toggleSidebar} />}
+
             <h1 className='header__secondary u-text-center'>Pasirinkite seniūniją</h1>
             <hr className='hr' />
 
--- Ka-veikia-seniunijos/ClientApp/src/components/Sidebar/Sidebar.jsx
+++ Ka-veikia-seniunijos/ClientApp/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 ﻿import React from 'react';
+import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import '../style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,6 +10,26 @@ export default function Sidebar({ onClose }) {
     return (
         <div className='sidebar'>
             <FontAwesomeIcon className='form__icon' icon={faXmark} onClick={onClose} />
+            <ul className='sidebar__list'>
+                <li className='sidebar__list--item'>
+                    <Button
+                    text='Prisijungti'
+                    styling='btn--sidebar'
+                    />
+                </li>
+                <li className='sidebar__list--item'>
+                    <Button
+                    text='Registruotis'
+                    styling='btn--sidebar'
+                    />
+                </li>
+                <li className='sidebar__list--item'>
+                    <Button
+                    text='Apie projektą'
+                    styling='btn--sidebar'
+                    />
+                </li>
+            </ul>
         </div>
     );
 }

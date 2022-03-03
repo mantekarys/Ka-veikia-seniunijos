54858b34972a8a23efb8270f225f8b910e19818c
79539642 <patrik.voicechovski@ktu.edu>
2022-03-03 20:15:51 +0200

    Added onClick funcionality to buttons in sidebar

--- Ka-veikia-seniunijos/ClientApp/src/components/Sidebar/Sidebar.jsx
+++ Ka-veikia-seniunijos/ClientApp/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
-﻿import React from 'react';
+﻿import React, { useContext } from 'react';
 import Button from '../Button/Button';
+import { GlobalContext } from '../Pages/Home/Context/GlobalState';
 import PropTypes from 'prop-types';
 import '../style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,26 +8,33 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Sidebar({ onClose }) {
+    const {
+        toggleLoginPopup,
+        toggleSignupPopup,
+    } = useContext(GlobalContext);
+
     return (
         <div className='sidebar'>
             <FontAwesomeIcon className='form__icon' icon={faXmark} onClick={onClose} />
             <ul className='sidebar__list'>
                 <li className='sidebar__list--item'>
                     <Button
-                    text='Prisijungti'
-                    styling='btn--sidebar'
+                        text='Prisijungti'
+                        styling='btn--sidebar'
+                        onClick={toggleLoginPopup}
                     />
                 </li>
                 <li className='sidebar__list--item'>
                     <Button
-                    text='Registruotis'
-                    styling='btn--sidebar'
+                        text='Registruotis'
+                        styling='btn--sidebar'
+                        onClick={toggleSignupPopup}
                     />
                 </li>
                 <li className='sidebar__list--item'>
                     <Button
-                    text='Apie projektą'
-                    styling='btn--sidebar'
+                        text='Apie projektą'
+                        styling='btn--sidebar'
                     />
                 </li>
             </ul>

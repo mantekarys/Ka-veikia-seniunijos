8500e28563d0b7cdffa63937a89c7571810f3bcc
79539642 <patrik.voicechovski@ktu.edu>
2022-03-07 20:16:47 +0200

    Added design for profile

--- Ka-veikia-seniunijos/ClientApp/src/components/UserProfile/UserProfile.jsx
+++ Ka-veikia-seniunijos/ClientApp/src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,6 @@
 ﻿import React, { useState } from 'react';
+import Input from '../Form/Input';
+import Button from '../Button/Button';
 import './_user-profile-styling.scss';
 import '../Utils/_utilities.scss';
 import '../Utils/_typography.scss';
@@ -6,7 +8,26 @@ import userProfile from '../../images/user-profile.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons'
 
+// If user type is resident - display dropdown
+
 export default function UserProfile() {
+    const [name, setName] = useState('Vardas');
+    const [surname, setSurname] = useState('Pavardė');
+    const [email, setEmail] = useState('Paštas');
+    const [isEditEnabled, setIsEditEnabled] = useState(false);
+
+    const hadleIconClick = (e) => {
+        e.target.classList.add('user-profile__icon--black');
+        e.target.closest('.user-profile__input-container').childNodes[0]
+            .classList.add('user-profile__input--edit');
+
+        setIsEditEnabled(true);
+    }
+
+    const handleOnSubmit = () => {
+        
+    }
+
     return (
         <div className='user-profile__container'>
             <div className='user-profile__header'>
@@ -21,10 +42,58 @@ export default function UserProfile() {
 
                 <div className='user-profile__card--right'>
                     <div className='user-profile__info-container'>
-                        <p className='paragraph'>Vardas <FontAwesomeIcon icon={faPen} /></p>
-                        <p className='paragraph'>Pavardė <FontAwesomeIcon icon={faPen} /></p>
-                        <p className='paragraph'>Paštas <FontAwesomeIcon icon={faPen} /></p>
+                        <div className='user-profile__input-container'>
+                            <Input
+                                styling='form__input user-profile__input'
+                                type='text'
+                                placeholder={name}
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                disabled={true}
+                            />
+                            <span>
+                                <FontAwesomeIcon icon={faPen} className='user-profile__icon' onClick={hadleIconClick} />
+                            </span>
+                        </div>
+
+                        <div className='user-profile__input-container'>
+                            <Input
+                                styling='form__input user-profile__input'
+                                type='text'
+                                placeholder={surname}
+                                value={surname}
+                                onChange={(e) => setSurname(e.target.value)}
+                                disabled={true}
+                            />
+                            <span>
+                                <FontAwesomeIcon icon={faPen} className='user-profile__icon' onClick={hadleIconClick} />
+                            </span>
+                        </div>
+
+                        <div className='user-profile__input-container'>
+                            <Input
+                                styling='form__input user-profile__input'
+                                type='text'
+                                placeholder={email}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                disabled={true}
+                            />
+                            <span>
+                                <FontAwesomeIcon icon={faPen} className='user-profile__icon' onClick={hadleIconClick} />
+                            </span>
+                        </div>
                     </div>
+
+                    {isEditEnabled &&
+                        <div className='user-profile__button-wrapper'>
+                        <Button
+                            text='Išsaugoti'
+                            styling='btn btn--user-profile'
+                            type='submit'
+                        />
+                    </div>
+                 }
                 </div>
             </div>
         </div>

aeae0fb9e34da5c56b34c257e049dff7ab9d793f
mantekarys <46086217+mantekarys@users.noreply.github.com>
2022-03-25 09:15:43 +0200

    pastrigau

---
+++ Ka-veikia-seniunijos/Controllers/EldershipController.cs
@@ -0,0 +1,60 @@
+﻿using Ka_veikia_seniunijos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ka_veikia_seniunijos.Controllers
+{
+    public class EldershipController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public EldershipController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]//make password hashed in database
+        public JsonResult Post(User user)
+        {
+            string query = @"select * from BSJ0CVGChE.User where Municipality='" + user.Municipality + "'";
+
+            string query2 = @"
+                    insert into BSJ0CVGChE.User values 
+                    ('" + user.Name + "," + user.LastName + "," + user.Email + "," + user.Municipality + "," + user.Password + "," + user.Name + @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);//check this somehow
+                    //if kaskkas myReader.Close();myCon.Close(); return new JsonResult("-2")
+
+                    //myReader.Close();
+                    //myCon.Close();
+                }
+
+                using (SqlCommand myCommand = new SqlCommand(query2, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("1");//arba Added Successfully
+        }
+    }
+}
---
+++ Ka-veikia-seniunijos/Controllers/UserController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Ka_veikia_seniunijos.Models;
+
+namespace Ka_veikia_seniunijos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select * from BSJ0CVGChE.User";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPut]
+        public JsonResult Put(User user)
+        {
+            string query = @"
+                    update BSJ0CVGChE.User set 
+                    Name = '" + user.Name + @"'
+                    LastName = '" + user.LastName + @"'
+                    Email = '" + user.Email + @"'
+                    Municipality = '" + user.Municipality + @"'
+                    Password = '" + user.Password + @"'
+                    where Id = " + user.Id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    try
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                    catch (Exception)
+                    {
+                        return new JsonResult("-1");
+                    }
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from  BSJ0CVGChE.User
+                    where Id = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+        [HttpPost]//make password hashed in database
+        public JsonResult Post(User user)
+        {
+            string query = @"
+                    insert into BSJ0CVGChE.User values 
+                    ('" + user.Name + "," + user.LastName + "," + user.Email + "," + user.Municipality + "," + user.Password + "," + user.Name +  @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    try
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                    }
+                    catch (Exception)
+                    {
+                        //paziurek kame problema
+                        throw;
+                    }
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("1");//arba Added Successfully
+        }
+    }
+}

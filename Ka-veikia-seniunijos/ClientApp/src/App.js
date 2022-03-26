229cee1c438599b151eac7b6af14a6d654410417
79539642 <patrik.voicechovski@ktu.edu>
2022-03-26 20:04:31 +0200

    Inbox implementation

--- Ka-veikia-seniunijos/ClientApp/src/App.js
+++ Ka-veikia-seniunijos/ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import Home from './components/Pages/Home/Home';
 import Eldership from './components/Pages/Eldership-feed/Eldership';
 import Profile from './components/Pages/Profile/Profile';
+import Mailbox from './components/Pages/Mailbox/Mailbox';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -13,6 +14,7 @@ export default class App extends Component {
               <Route exact path='/:path(|home)' component={Home} />
               <Route exact path='/profile' component={Profile} />
               <Route exact path='/eldership' component={Eldership} />
+              <Route exact path='/mailbox' component={Mailbox} />
               {/* <Route exact path='/about' component={About} /> */}
         </>
     );
---
+++ Ka-veikia-seniunijos/ClientApp/src/components/Inbox/Inbox.jsx
@@ -0,0 +1,89 @@
+﻿import React, { useState } from 'react';
+import './_inbox-style.scss';
+import '../Utils/_utilities.scss';
+import '../Utils/_typography.scss';
+
+export default function Inbox() {
+    const [activeTab, setActiveTab] = useState('reveived');
+
+    return (
+        <div className="inbox__container">
+            <div className="inbox__tabs">
+                <span
+                    className={`inbox__tabs-name ${activeTab === 'reveived' ? 'inbox__tabs-name--active' : ''}`}
+                    onClick={() => setActiveTab('reveived')}
+                >
+                    Gautos žinutės
+                </span>
+                <span
+                    className={`inbox__tabs-name ${activeTab === 'sent' ? 'inbox__tabs-name--active' : ''}`}
+                    onClick={() => setActiveTab('sent')}
+                >
+                    Išsiųstos žinutės
+                </span>
+            </div>
+
+            <div className="inbox__content">
+                <div className="inbox__content-item">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item inbox__content-item--black">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item inbox__content-item--black">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item inbox__content-item--black">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item inbox__content-item--black">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+
+                <div className="inbox__content-item inbox__content-item--black">
+                    <span className="message-sender">Vilniaus seniūnija</span>
+                    <span className="message-topic">Atsakymas</span>
+                    <span className="message-date">03-22</span>
+                </div>
+            </div>
+        </div>
+    );
+}

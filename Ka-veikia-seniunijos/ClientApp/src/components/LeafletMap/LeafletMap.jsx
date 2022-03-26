45f4158d71993e4850a522ab31a84ad0eb56d1d7
79539642 <patrik.voicechovski@ktu.edu>
2022-03-26 22:53:02 +0200

    leaflet lib integration

--- Ka-veikia-seniunijos/ClientApp/src/components/LeafletMap/LeafletMap.jsx
+++ Ka-veikia-seniunijos/ClientApp/src/components/LeafletMap/LeafletMap.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+﻿import React, { useState } from 'react';
 import './_leaflet-map-style.scss';
 import { MapContainer, TileLayer } from 'react-leaflet';
-
+import { Switch } from 'pretty-checkbox-react';
 
 export default function LeafletMap({ lat, lng }) {
+    const [showEvents, setShowEvents] = useState(true);
+    const [showPlaces, setShowPlaces] = useState(true);
+
+    const handleOnEventChange = () => {
+        setShowEvents((showEvents) => setShowEvents(!showEvents));
+    }
+
+    const handleOnPlacesChange = () => {
+        setShowPlaces((showPlaces) => setShowPlaces(!showPlaces));
+    }
+
     return (
         <div className="map__container">
+            <div className="map__header">
+                <h1 className="header__primary">Interaktyvus žemėlapis</h1>
+                <p className="paragraph--large">Žemėlapis, kuriame galite rasti visus seniūnijų renginius ir lankytinas vietas</p>
+
+                <Switch
+                    color='primary'
+                    shape='fill'
+                    style={{ fontSize: '15px' }}
+                    onChange={handleOnEventChange}
+                    checked={showEvents}
+                >
+                    <b>Renginiai</b>
+                </Switch>
+                <Switch
+                    color='primary'
+                    shape='fill'
+                    style={{ fontSize: '15px' }}
+                    onChange={handleOnPlacesChange}
+                    checked={showPlaces}
+                >
+                    <b>Lankytinos vietos</b>
+                </Switch>
+            </div>
+
             <MapContainer center={[lat, lng]} zoom={13}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'

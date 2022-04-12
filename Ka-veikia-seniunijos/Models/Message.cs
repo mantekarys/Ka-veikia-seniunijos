e7cd6e90c665454f580129404be6bf9198c33e8f
mantekarys <46086217+mantekarys@users.noreply.github.com>
2022-04-12 20:36:58 +0300

    gal ir geras

---
+++ Ka-veikia-seniunijos/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ka_veikia_seniunijos.Models
+{
+    public class Message
+    {
+        public string Sender { get; set; }//toks tik kai vienas user o kitas eldership
+        public string SenderType { get; set; }
+        public string Receiver { get; set; }
+        public string ReceiverType { get; set; }
+        public string Topic { get; set; }
+        public string Date { get; set; }
+        public string Text { get; set; }
+        public bool Reply { get; set; }
+        public bool Received { get; set; }
+        public int Fk_user { get; set; }
+        public int Fk_eldership { get; set; }
+    }
+}

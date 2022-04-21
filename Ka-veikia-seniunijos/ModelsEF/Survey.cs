12e85745e949d580da5958e11bab520c625c5ae3
RedasNiuklys <redas.niuklys@ktu.edu>
2022-04-21 19:07:10 +0300

    First commit

---
+++ Ka-veikia-seniunijos/ModelsEF/Survey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Ka_veikia_seniunijos.ModelsEF
+{
+    public partial class Survey
+    {
+        public Survey()
+        {
+            Question = new HashSet<Question>();
+        }
+
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Eldership { get; set; }
+
+        public virtual ICollection<Question> Question { get; set; }
+    }
+}

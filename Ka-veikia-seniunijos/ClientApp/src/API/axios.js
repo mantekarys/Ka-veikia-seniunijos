1d33a09da4e8f34d3cabca0c19e22ca3cd805268
Patrikas Voicechovski <patrikasvoicechovski@gmail.com>
2022-04-23 13:19:22 +0300

    Address to coordinates covertion implementation using position track API

--- Ka-veikia-seniunijos/ClientApp/src/API/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const baseURL = 'http://localhost:5000'
-export const apiClient = axios.create({
-    baseURL,
-    headers: {
-      "Content-type": "application/json",
-    },
-  });

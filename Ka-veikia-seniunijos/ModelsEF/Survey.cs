c7159c2d53437b21fd769df4d0f9d0bbcb8157fe
mantekarys <46086217+mantekarys@users.noreply.github.com>
2022-05-04 19:34:42 +0300

    changed to postDate

--- Ka-veikia-seniunijos/ModelsEF/Survey.cs
+++ Ka-veikia-seniunijos/ModelsEF/Survey.cs
@@ -17,7 +17,7 @@ namespace Ka_veikia_seniunijos.ModelsEF
         public int Id { get; set; }
         public string Name { get; set; }
         public int EldershipFk { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime PostDate { get; set; }
 
         public virtual Eldership EldershipFkNavigation { get; set; }
         public virtual ICollection<Question> Question { get; set; }

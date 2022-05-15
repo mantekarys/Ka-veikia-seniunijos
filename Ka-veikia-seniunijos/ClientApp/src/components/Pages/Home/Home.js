03a8f6fdcca4823eb3e6927cb25e77d1e518b085
Patrikas Voicechovski <patrikasvoicechovski@gmail.com>
2022-05-15 11:29:29 +0300

    home page redesign

--- Ka-veikia-seniunijos/ClientApp/src/components/Pages/Home/Home.js
+++ Ka-veikia-seniunijos/ClientApp/src/components/Pages/Home/Home.js
@@ -3,15 +3,16 @@ import HomeHeader from '../../Header/HomeHeader';
 import UserHeader from '../../Header/UserHeader';
 import HomeContent from './HomeContent';
 import Footer from '../../Footer/Footer';
+import { GlobalProvider } from "../../Header/Context/HeaderState";
 
 function Home() {
 
     return (
-        <>
+        <GlobalProvider>
             { sessionStorage['userData'] ? <UserHeader /> : <HomeHeader /> }
             <HomeContent />
             <Footer />
-        </> 
+        </GlobalProvider>
     );
 }
 
